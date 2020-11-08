91fddb440411b8c6471db8580c2ea14cb2341de1
Jessica Cooper <jessicagcooper@gmail.com>
2020-11-08 17:42:31 -0600

    Added 3 basic user routes. Formatting.

--- users/users-model.js
+++ users/users-model.js
@@ -1,33 +1,33 @@
 import db from '../db/db-config';
 
 module.exports = {
-    add,
-    find,
-    findBy,
-    findById
+  add,
+  find,
+  findBy,
+  findById,
 };
 
 //basic functions here
 function find() {
-    return db('users')
-        .select('id', 'username')
-        .orderBy('id');
+  return db('users')
+    .select('id', 'username')
+    .orderBy('id');
 }
 
 function findBy(filter) {
-return db('users')
+  return db('users')
     .where(filter);
 }
 
 function findById(id) {
-return db('users')
+  return db('users')
     .select('id', 'username')
     .where({ id })
     .first();
 }
 
 async function add(user) {
-const [id] = await db('users').insert(user, "id");
+  const [id] = await db('users').insert(user, 'id');
 
-return findById(id);
+  return findById(id);
 }
--- users/users-router.js
+++ users/users-router.js
@@ -1,6 +1,46 @@
 const router = require('express').Router();
 
 const Users = require('./users-model.js');
+const restricted = require('../auth/restricted_middleware.js');
 
-//write routes here
-//need auth middleware
+router.get('/users', restricted, (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => res.send(err));
+});
+
+router.get('/users/current', restricted, (req, res) => {
+  Users.find()
+    // eslint-disable-next-line no-unused-vars
+    .then((user) => {
+      let temp = req.user;
+      console.log(req.user);
+      res.json({ currentUserId: temp.id, currentUsername: temp.username });
+    })
+    .catch((err) => res.send(err));
+});
+
+router.get('/users/:id', restricted, (req, res) => {
+  const id = req.params.id;
+
+  Users.findById(id)
+    .then((user) => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({
+          message: 'The user with the specified id could not be found.',
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: 'The server could not retrieve the user. Error on server end.',
+        error,
+      });
+    });
+});
+
+module.exports = router;
