1df1ebdb20cbe161dd69bec935b918e4b5b1c3fe
Jessica Cooper <jessicagcooper@gmail.com>
2020-11-08 17:14:25 -0600

    Created basic auth middleware based on JWT library.

---
+++ auth/restricted_middleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token) {
+    const secret = process.env.JWT_SECRET;
+
+    jwt.verify(token, secret, function (err, decodedToken) {
+      if (err) {
+        res.status(401).json({ message: 'The credentials are invalid', err });
+      } else {
+        req.user = decodedToken;
+        console.log(req.user);
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: 'No token provided.' });
+  }
+};
